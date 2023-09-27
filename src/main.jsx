039d4686d29df5a5315476635963890354251a84
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-27 20:16:53 -0300

    style global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Details } from './pages/Details';
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme';
+import GlobalStyles from './styles/global'; 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Details />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
