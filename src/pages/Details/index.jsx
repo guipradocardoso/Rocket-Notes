856f7ec4629fbffae43b69d2aa2d879ccaf14161
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-28 06:53:34 -0300

    add component Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles"
 import { Header } from "../../components/Header"
 import { Buttton } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 
 
@@ -18,6 +19,13 @@ export function Details() {
         </Links>
 
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="Node" />
+      </Section>
+        
+
      
       <Buttton title="Voltar" />
     </Container>
