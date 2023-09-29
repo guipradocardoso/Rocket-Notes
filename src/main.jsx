68d6cb375c230d053f595c0448b92c73650a2c1f
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-29 20:16:26 -0300

    add page SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
-import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 
 
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 );
