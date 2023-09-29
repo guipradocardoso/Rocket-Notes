8d417ae0a333e28425c0a6804f62a042bc2831e0
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-29 13:35:34 -0300

    add button NewNote

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
    return (
@@ -13,8 +15,12 @@ export function Home() {
 
 
          <Menu>
-
+            <li><ButtonText title="Todos" isActive /></li>
+            <li><ButtonText title="Frontend" /></li>
+            <li><ButtonText title="Node" /></li>
+            <li><ButtonText title="React" /></li>
          </Menu>
+
          <Search>
 
          </Search>
@@ -24,8 +30,9 @@ export function Home() {
          </Content>
 
          <NewNote>
-
+            <FiPlus />
+            Criar nota
          </NewNote>
-      </Container>
+      </Container >
    );
 }
