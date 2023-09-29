1de177b12e6e3d3bb4556a80c23194b16f7f0ed1
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-29 14:56:04 -0300

    SignIn Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Home } from './pages/Home';
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
+import { SignIn } from './pages/SignIn';
 
 
 
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 );
