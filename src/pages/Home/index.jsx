744e20cea51fa200deda81d3d9ee03d86f523960
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-29 14:32:55 -0300

    add component Note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
-import { Header } from '../../components/Header';
-import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
+import { Note } from '../../components/Note';
 
 export function Home() {
    return (
@@ -28,6 +30,16 @@ export function Home() {
          </Search>
 
          <Content>
+            <Section title="Minhas notas">
+               <Note data={{
+                  title: 'React',
+                  tags: [
+                     { id: '1', name: 'react' },
+                     { id: '2', name: 'rocketseat' }
+                  ]
+               }}></Note>
+
+            </Section>
 
          </Content>
 
