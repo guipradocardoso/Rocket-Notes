d606e86620966a2b3315cff5d49253c366757727
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-31 20:21:01 -0300

    register note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -7,14 +7,22 @@ import { Section } from '../../components/Section';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 
+import { api } from '../../../../backend-nodejs-explorer/src/services/api';
+
 import { Container, Form } from './styles';
 
 export function New() {
+
+   const [title, setTitle] = useState("");
+   const [description, setDescription] = useState("");
+
    const [links, setLinks] = useState([]);
    const [newLink, setNewLink] = useState("");
 
    const [tags, setTags] = useState([]);
    const [newTag, setNewTag] = useState("");
+
+   const navigate = useNavigate();
 
 
    function handleAddLink() {
@@ -36,6 +44,20 @@ export function New() {
       setTags(prevState => prevState.filter(tag => tag !== deleted));
    }
 
+   async function handleNewNote() {
+      await api.post("/notes", {
+         title,
+         description,
+         tags,
+         links
+      });
+
+      alert("Nota criada com sucesso!");
+
+      navigate("/");
+
+   }
+
    return (
       <Container>
          <Header />
@@ -47,8 +69,14 @@ export function New() {
                   <Link to="/">voltar</Link>
                </header>
 
-               <Input placeholder="Título" />
-               <Textarea placeholder="Observações" />
+               <Input
+                  placeholder="Título"
+                  onChange={e => setTitle(e.target.value)}
+               />
+               <Textarea
+                  placeholder="Observações"
+                  onChange={e => setDescription(e.target.value)}
+               />
 
                <Section title="Links úteis">
                   {
@@ -92,7 +120,10 @@ export function New() {
                      />
                   </div>
                </Section>
-               <Button title="Salvar" />
+               <Button
+                  title="Salvar"
+                  onClick={handleNewNote}
+               />
             </Form>
          </main>
       </Container>
