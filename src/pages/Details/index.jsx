fad69e65c7b053624cd01a4dd51ae4f0a1e602db
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-11-07 23:01:58 -0300

    Loading note details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,44 +1,97 @@
-import { Container, Links, Content } from "./styles"
+import { useState, useEffect } from 'react';
+import { Container, Links, Content } from "./styles";
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { Tag } from "../../components/Tag"
-import { Button } from "../../components/Button"
-import { Header } from "../../components/Header"
-import { Section } from "../../components/Section"
-import { ButtonText } from "../../components/ButtonText"
+import { Tag } from "../../components/Tag";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
+
+import { api } from '../../../../backend-nodejs-explorer/src/services/api';
 
 
 
 export function Details() {
+
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/");
+
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data)
+    }
+
+    fetchNote();
+  }, []);
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
-          <h1>
-            Introdução ao React
-          </h1>
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
+            <h1>
+              {data.title}
+            </h1>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, nemo atque eveniet pariatur explicabo doloremque perspiciatis a illo tenetur labore rem cupiditate voluptate omnis necessitatibus! Dolorem modi expedita nisi rem.
-          </p>
+            <p>
+              {data.description}
+            </p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-            </Links>
-          </Section>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      < li key={String(link.id)} >
+                        <a href={link.url} target="_blank" rel="noreferrer">
+                          {link.url}
+                        </a>
+                      </li>
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="Node" />
-          </Section>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
 
-          <Button title="Voltar" />
-        </Content>
-      </main>
-    </Container>
+
+
+            {
+              data.tags &&
+
+              <Section title="Marcadores">
+
+                {
+                  data.tags.map(tag => (
+
+                    <Tag
+                      key={String(tag.id)}
+                      title={tag.name} />
+                  ))
+                }
+              </Section>
+            }
+
+            <Button
+              title="Voltar"
+              onClick={handleBack}
+            />
+          </Content>
+        </main>}
+    </Container >
   )
 }
