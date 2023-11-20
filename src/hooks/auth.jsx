ec864623881a5a2b6acc24dbe4ef7a8e4e042786
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-11-20 19:53:08 -0300

    updated import

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
-import { api } from '../../../backend-nodejs-explorer/src/services/api';
+import { api } from '../services/api';
 
 export const AuthContext = createContext({});
 
